3e6b6b0276a132c4007df8192c3d3f0e1869dee4
kajal shukla <shuklakajal1703@gmail.com>
2025-03-20 20:16:26 +0530

    Update HW-13.js

--- HW-13.js
+++ HW-13.js
@@ -1,7 +1,7 @@
 // correction task
 
 function myEligibility(age){
-    return age > 18 ? "eligiable to vote" : "not eligiable";
+    return age >= 18 ? "eligiable to vote" : "not eligiable";
 }
 console.log(myEligibility(20));
 console.log(myEligibility(16));
